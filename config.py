189bb5f1cd812df1588e370c84431a7c6b097ce5
joshunrau <contact@joshuaunrau.com>
2023-06-07 17:06:55 -0400

    add validation with no library

--- config.py
+++ config.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 import logging
 import sys
-from typing import TypedDict
+
+from typing import TypedDict, Any, get_args, get_origin, get_type_hints, cast
+from types import UnionType
 
 import yaml
 
+from validation import all_items_match_type, get_type_name, parse_list_item_types
 
 class Config(TypedDict):
     """defines the structure of the config dict to be passed to the function extract_UKBB_tabular_data"""
@@ -47,7 +50,7 @@ class Config(TypedDict):
 
     # List of string responses to map to Null
     drop_null_strings: list[str] | list[None]
-    drop_null_numerics: list[float] | list[None]
+    drop_null_numerics: list[float | int] | list[None]
 
     ## Wide output control
 
@@ -64,23 +67,51 @@ class Config(TypedDict):
     # When recode_wide_column_valuetype=true some values from recode_data_values=true
     # # some values will break setting column datatypes
     # # Substitute strings to values set below
-    convert_less_than_value_integer: int
-    convert_less_than_value_continuous: int | float
-
+    convert_less_than_value_integer: int | None
+    convert_less_than_value_continuous: int | float | None
 
 def load_config(config_file: str) -> Config:
     """Returns a config dict loaded from a YAML file. It is assumed the format is correct"""
+
+    # Load the data from the file
     try:
         with open(config_file, "r") as stream:
-            try:
-                tempdict = yaml.safe_load(stream)
-                for key in tempdict.keys():
-                    if isinstance(tempdict[key], list):
-                        tempdict[key] = [i for i in tempdict[key] if i is not None]
-                return tempdict
-            except yaml.YAMLError as exc:
-                logging.exception(exc)
-                sys.exit(1)
-    except FileNotFoundError as exc:
+            data = yaml.safe_load(stream)
+    except (FileNotFoundError, yaml.YAMLError) as exc:
         logging.exception(exc)
         sys.exit(1)
+    
+    # Validate the format of the loaded data
+    if not isinstance(data, dict):
+        raise TypeError(f"Invalid format of configuration file '{config_file}', received type '{get_type_name(data)}' not 'dict'")
+    
+    for key, value_type in get_type_hints(Config).items():
+
+        # Make sure the key exists in the config
+        if key not in data:
+            raise ValueError(f"Config object does not contain required key: '{key}'")
+
+        # Now, we can make sure the type is correct
+        if isinstance(value_type, UnionType):
+            valid_types = tuple(get_origin(x) or x for x in get_args(value_type))
+        else:
+            valid_types = value_type
+        
+        # See if the type is one of the possible base types (e.g, list, string)
+        is_base_type = isinstance(data[key], valid_types)
+        if not is_base_type:
+            raise TypeError(f"Invalid type for '{key}', expected one of {[x.__name__ for x in valid_types]}, got '{get_type_name(data[key])}'")
+        
+        if isinstance(data[key], list):
+            assert isinstance(value_type, UnionType), "Unless something changes, all lists are union type"
+
+            # We need to get all of the possible types for the items in the list
+            valid_item_types = parse_list_item_types(value_type)
+            
+            # For each type, all of the items in the list must be that type
+            all_items_match_type(data[key], valid_item_types)
+
+            # Filter none in any case, as this was the behavior written by Gabe before
+            data[key] = [i for i in data[key] if i is not None]
+    
+    return cast(Config, data)
---
+++ validation.py
@@ -0,0 +1,28 @@
+from typing import Any, get_args
+from types import NoneType, UnionType
+
+def get_type_name(obj: object) -> str:
+    return obj.__class__.__name__
+
+
+def parse_list_item_types(union_type: UnionType) -> list[type]:
+    """ given a union of list types, return a list of all of the values contained in the union members """
+    item_types: list[type] = []
+    for list_type in get_args(union_type):
+        for item_type in get_args(list_type):
+            if item_type not in item_types:
+                item_types.append(item_type)
+    return item_types
+    
+
+def all_items_match_type(items: list[Any], types: list[type]) -> None:
+    """ throw if not all of the items in the list are instances one of the types, where mixed types in the list are not allowed """
+    for item_type in types:
+        if item_type is None:
+            item_type = NoneType
+        for i, item in enumerate(items):
+            if not isinstance(item, item_type):
+                break
+            elif i == len(items) - 1:
+                return
+    raise TypeError(f"List must be list of one of types: '{types}', got '{items}'")
